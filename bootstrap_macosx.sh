100e96ec20530bd25fa22b204ab177a9e5d22c20
David Saenz <dsaenz@bebanjo.com>
2016-03-30 15:56:02 +0200

    Disable the Dashboard

--- bootstrap_macosx.sh
+++ bootstrap_macosx.sh
@@ -1,1 +1,4 @@
 sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "Please return to David +34695680107"
+
+# Disable the Dashboard
+defaults write com.apple.dashboard mcx-disabled -boolean YES && killall Dock
